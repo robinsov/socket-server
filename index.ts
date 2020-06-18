6641bd5a30a56768c50947ec5973580874b82c2b
unknown <robdjmixeo@gmail.com>
2020-06-18 15:36:11 -0500

    seccion 4

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 
 
-const server = new Server();
+const server = Server.instance;
 //bodyparser
 server.app.use(bodyParser.urlencoded({extended: true}));
 server.app.use(bodyParser.json());
